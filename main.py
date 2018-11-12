3e5c5c53b3c92c015f8aee6b3134684c785b690b
DomNidy <dominicnidy3@gmail.com>
2018-11-12 11:02:22 -0500

    Intial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+import pygame
+import time
+import random
+
+pygame.display.init()
+pygame.font.init()
+screen_height = 1600
+screen_width = 900
+screen = pygame.display.set_mode([screen_height, screen_width])
+pygame.display.set_caption("Boink")
+
+run = True
+while run:
+      pygame.time.delay(1)

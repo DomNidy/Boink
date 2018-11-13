0fe1c9ce6c4f726419d3ace0c4a0767cf0ecdccc
DomNidy <dominicnidy3@gmail.com>
2018-11-12 21:30:35 -0500

    Added credits screen and organized code with comments

--- main.py
+++ main.py
@@ -8,14 +8,26 @@ screen_height = 1600
 screen_width = 900
 screen = pygame.display.set_mode([screen_height, screen_width])
 pygame.display.set_caption("Boink")
+
+#Images
 background = pygame.image.load("background.jpg").convert()
+credits_screen = pygame.image.load("credits_screen.png").convert()
 
 #Class Definitions
 class Character:
-      pass
+      x = 50
+      y = 50
+      width = 60
+      height = 80
+      vel = 1
 
 class Enemy:
       pass
+
+#Function Definitions
+def load_image(image):
+      screen.blit(image, (0, 0))
+
 
 run = True
 while run:
@@ -29,9 +41,18 @@ while run:
       for event in pygame.event.get():
             if event.type == pygame.QUIT:
                   pygame.quit()
+
+      #Creates a list of all keys
+      keys = pygame.key.get_pressed()
+
+      #If "E" key is pressed display e_image
+      if keys[pygame.K_TAB]:
+            load_image(credits_screen)
       
       #The display update should be at the end of the code            
       pygame.display.update()
+
+pygame.quit()
 
       
       

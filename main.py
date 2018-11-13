f0bb6c453923f28f48bd29050fc29acd52146b64
DomNidy <dominicnidy3@gmail.com>
2018-11-12 19:08:17 -0500

    Added background and quit functionality

--- main.py
+++ main.py
@@ -8,7 +8,30 @@ screen_height = 1600
 screen_width = 900
 screen = pygame.display.set_mode([screen_height, screen_width])
 pygame.display.set_caption("Boink")
+background = pygame.image.load("background.jpg").convert()
+
+#Class Definitions
+class Character:
+      pass
+
+class Enemy:
+      pass
 
 run = True
 while run:
+      #Restarts the loop every 1 milli second
       pygame.time.delay(1)
+      
+      #Adds the background.jpg as the background
+      screen.blit(background, (0, 0))
+      
+      #If the "X" button is pressed the game will end
+      for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                  pygame.quit()
+      
+      #The display update should be at the end of the code            
+      pygame.display.update()
+
+      
+      
